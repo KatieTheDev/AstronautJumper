9741bb162521b14a7dc28e35a2e28e67e8a63f2b
KatieTheDev <contact@katietso.dev>
2021-09-29 12:50:44 -0600

    Creating more logic in GameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public bool isAlive = true;
+    public int lives = 3;
+    public int levelNumber = 1;
+    public int score = 0;
+
+    public GameObject enemyAmmoPrefab;
+
+    private Vector3[] spawnLocation;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("ChangeScore", 0.25f, 0.25f); // Add 1 to score about every 1/4th of a second
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (lives < 1)
+        {
+            isAlive = false;
+        }
+    }
+
+    void ChangeScore(int scoreChange = 1)
+    {
+        if (isAlive)
+        {
+            score += scoreChange;
+        }
+    }
+
+    Vector3 RemoveLife()
+    {
+        lives--;
+        return spawnLocation[levelNumber];
     }
 }
